2a81ccdc325c740843aa9b864c4b79575b26694b
usmanfast465 <i190465@nu.edu.pk>
2023-09-15 15:35:15 +0500

    Next iter

---
+++ app/src/main/java/com/mirzausmanbaig/i190465/chat_screen.java
@@ -0,0 +1,14 @@
+package com.mirzausmanbaig.i190465;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class chat_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_screen);
+    }
+}
---
+++ app/src/main/java/com/mirzausmanbaig/i190465/conversation.java
@@ -0,0 +1,14 @@
+package com.mirzausmanbaig.i190465;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class conversation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conversation);
+    }
+}
